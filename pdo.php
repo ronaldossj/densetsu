43785bcb5ac9e4371096430dd0ffb07a94e1cab6
Leonardo Santos <leonardo.santos@i9xp.com.br>
2020-06-14 16:44:51 -0300

    alguns arquivos modificados e adicionado o pdo

---
+++ pdo.php
@@ -0,0 +1,20 @@
+<?php
+
+// Conexao com MySQL via PDO
+$db_dsn = 'mysql:host=banco.local.com.br;port=3306;dbname=densetsu';
+$db_usuario = 'root';
+$db_senha = 'root';
+$db_opcoes = array(
+    PDO::ATTR_PERSISTENT => false,
+    PDO::ATTR_CASE => PDO::CASE_NATURAL,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+);
+
+try {
+    $pdo = new PDO($db_dsn, $db_usuario, $db_senha, $db_opcoes);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, 2);
+} catch (PDOException $e) {
+    trigger_error($e->getMessage());
+}
+
